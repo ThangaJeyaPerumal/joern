24ae6870960bbce827e24082455fcb0d20c3a0b5
Markus Lottmann <markus.lottmann@mlbox.net>
2024-10-09 14:34:45 +0200

    Incorporate review comments.
    - No more special case for handling `kotlin.Function`.
    - Removed some unused code.
    - Stop using currentTimeMillis.

--- joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/types/TypeRenderer.scala
+++ joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/types/TypeRenderer.scala
@@ -127,7 +127,6 @@ class TypeRenderer(val keepTypeArguments: Boolean = false) {
     val rendered =
       if (t.isInstanceOf[ErrorType]) TypeConstants.any
       else if (TypeUtilsKt.isTypeParameter(t)) TypeConstants.javaLangObject
-      else if (isFunctionXType(t)) TypeConstants.kotlinFunctionXPrefix + (t.getArguments.size() - 1).toString
       else
         Option(TypeUtils.getClassDescriptor(t))
           .map { descriptor =>
@@ -153,13 +152,6 @@ class TypeRenderer(val keepTypeArguments: Boolean = false) {
     } else {
       renderedType
     }
-  }
-
-  private def isFunctionXType(t: KotlinType): Boolean = {
-    val renderer            = descriptorRenderer()
-    val renderedConstructor = renderer.renderTypeConstructor(t.getConstructor)
-    renderedConstructor.startsWith(TypeConstants.kotlinFunctionXPrefix) ||
-    renderedConstructor.startsWith(TypeConstants.kotlinSuspendFunctionXPrefix)
   }
 
   def stripped(typeName: String): String = {
